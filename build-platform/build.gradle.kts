e8de3cda48652d234706e5ef20a481661b9a8ac1
Bo Zhang <bo@gradle.com>
2021-12-10 11:48:17 +0800

    Bump log4j to latest 2.15.0
    To address CVE-2021-44228

--- build-platform/build.gradle.kts
+++ build-platform/build.gradle.kts
@@ -10,6 +10,8 @@ dependencies {
     api(platform("org.springframework.boot:spring-boot-dependencies:2.4.4"))
 
     constraints {
+        api("org.apache.logging.log4j:log4j-api:2.15.0")
+        api("org.apache.logging.log4j:log4j-to-slf4j:2.15.0")
         api("org.flywaydb:flyway-core:7.7.1")
         api("org.jetbrains.teamcity:teamcity-rest-client:1.14.0")
     }
