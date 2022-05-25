5108ae6e75210e495db0bd1f4a274d1ad03ffb24
David M. Carr <dcarr@gradle.com>
2022-05-25 14:14:26 +0200

    Progress on renderer

--- application/src/main/kotlin/org/gradle/devprod/collector/api/BuildScanRenderer.kt
+++ application/src/main/kotlin/org/gradle/devprod/collector/api/BuildScanRenderer.kt
@@ -2,7 +2,9 @@ package org.gradle.devprod.collector.api
 
 import com.slack.api.methods.request.chat.ChatUnfurlRequest.UnfurlDetail
 import org.gradle.devprod.collector.model.BuildScanSummary
+import kotlin.time.ExperimentalTime
 
+@ExperimentalTime
 interface BuildScanRenderer {
     fun render(buildScanSummary: BuildScanSummary) : UnfurlDetail
 }
