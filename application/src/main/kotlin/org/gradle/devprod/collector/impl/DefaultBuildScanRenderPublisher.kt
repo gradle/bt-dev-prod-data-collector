b149a71ad94a3725767dae5d5b18a437cef30aa3
Jim Hurne <jhurne@gradle.com>
2022-05-25 14:54:17 +0200

    Implement publisher (no tests)

--- application/src/main/kotlin/org/gradle/devprod/collector/impl/DefaultBuildScanRenderPublisher.kt
+++ application/src/main/kotlin/org/gradle/devprod/collector/impl/DefaultBuildScanRenderPublisher.kt
@@ -1,12 +1,31 @@
 package org.gradle.devprod.collector.impl
 
+import com.slack.api.Slack
+import com.slack.api.methods.request.chat.ChatUnfurlRequest
+import com.slack.api.methods.request.chat.ChatUnfurlRequest.ChatUnfurlRequestBuilder
 import com.slack.api.methods.request.chat.ChatUnfurlRequest.UnfurlDetail
 import org.gradle.devprod.collector.api.BuildScanRenderPublisher
 import org.springframework.stereotype.Service
 
 @Service
 class DefaultBuildScanRenderPublisher : BuildScanRenderPublisher {
+    private val slack = Slack.getInstance()
+    private val token = System.getenv("SLACK_API_TOKEN")
+
     override fun publish(channel: String, timeStamp: String, unfurledLinks: Map<String, UnfurlDetail>) {
-        TODO("Not yet implemented")
+        val methods = slack.methods(token)
+
+        val request = ChatUnfurlRequest.builder()
+            .channel(channel)
+            .ts(timeStamp)
+            .unfurls(unfurledLinks)
+            .build()
+
+        val response = methods.chatUnfurl(request)
+        if (response.isOk) {
+            println("Unfurled message request was successful.")
+        } else {
+            println("Unfurled message request failed: ${response.error}. Full response: $response")
+        }
     }
 }
