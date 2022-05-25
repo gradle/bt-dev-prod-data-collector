e66d7655bd0d8b6dcf5b999a0698a74e993dd4d6
Bo Zhang <bo@gradle.com>
2022-05-25 20:34:37 +0800

    Add a regex to recognize build scan summary url

--- application/src/main/kotlin/org/gradle/devprod/collector/impl/DefaultLinkSharedHandler.kt
+++ application/src/main/kotlin/org/gradle/devprod/collector/impl/DefaultLinkSharedHandler.kt
@@ -6,12 +6,19 @@ import org.gradle.devprod.collector.model.LinkSharedEvent
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 
+internal val summaryPathRegex = "/s/\\w+".toRegex()
+
 @Service
 class DefaultLinkSharedHandler @Autowired constructor(private val buildScanSummaryService: BuildScanSummaryService) : LinkSharedHandler {
     override fun handleBuildScanLinksShared(linkSharedEvent: LinkSharedEvent) {
         linkSharedEvent.links.forEach {
-            val buildScanId = it.url.path.toString().substring("/s".length)
-            println(buildScanSummaryService.getSummary(it.domain, buildScanId))
+            val path = it.url.path.toString()
+            if (path.matches(summaryPathRegex)) {
+                val buildScanId = it.url.path.toString().substring("/s".length)
+                println(buildScanSummaryService.getSummary(it.domain, buildScanId))
+            } else {
+                println("Unsupported url: ${it.url}")
+            }
         }
     }
 }
