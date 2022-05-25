ceac998ddf39d9814038cc9419326d7e7cf09208
Bo Zhang <bo@gradle.com>
2022-05-25 20:21:13 +0800

    Remove unnecessary println

--- application/src/main/kotlin/org/gradle/devprod/collector/impl/DefaultLinkSharedHandler.kt
+++ application/src/main/kotlin/org/gradle/devprod/collector/impl/DefaultLinkSharedHandler.kt
@@ -9,7 +9,6 @@ import org.springframework.stereotype.Service
 @Service
 class DefaultLinkSharedHandler @Autowired constructor(private val buildScanSummaryService: BuildScanSummaryService) : LinkSharedHandler {
     override fun handleBuildScanLinksShared(linkSharedEvent: LinkSharedEvent) {
-        println("Handling event: $linkSharedEvent")
         linkSharedEvent.links.forEach {
             val buildScanId = it.url.path.toString().substring("/s".length)
             println(buildScanSummaryService.getSummary(it.domain, buildScanId))
