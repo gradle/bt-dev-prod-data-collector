10c6ab6de5db8e02a670f81a6a9893ebf9dbeb70
Bo Zhang <bo@gradle.com>
2022-05-25 20:15:40 +0800

    Invoke buildScanSummaryService

--- application/src/main/kotlin/org/gradle/devprod/collector/impl/DefaultLinkSharedHandler.kt
+++ application/src/main/kotlin/org/gradle/devprod/collector/impl/DefaultLinkSharedHandler.kt
@@ -1,14 +1,19 @@
 package org.gradle.devprod.collector.impl
 
+import org.gradle.devprod.collector.api.BuildScanSummaryService
 import org.gradle.devprod.collector.api.LinkSharedHandler
 import org.gradle.devprod.collector.model.LinkSharedEvent
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 
 @Service
-class DefaultLinkSharedHandler : LinkSharedHandler {
-
+class DefaultBuildScanLinkSharedHandler @Autowired constructor(private val buildScanSummaryService: BuildScanSummaryService) : LinkSharedHandler {
     override fun handleBuildScanLinksShared(linkSharedEvent: LinkSharedEvent) {
         println("Handling event: $linkSharedEvent")
+        linkSharedEvent.links.forEach {
+            val buildScanId = it.url.path.toString().substring("/s".length)
+            println(buildScanSummaryService.getSummary(it.domain, buildScanId))
+        }
     }
 
 }
