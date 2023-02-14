f1377932280f2492ce9328bff285fe3744a73a30
Balint Hegyi <bhegyi@gradle.com>
2023-02-14 10:40:26 +0000

    Enable debugging

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: java -Xmx160M -XX:MaxMetaspaceSize=128M -jar application/build/libs/application-*.jar
+web: java -agentlib:jdwp=transport=dt_socket,server=y,address=9090,suspend=y -Xmx160M -XX:MaxMetaspaceSize=128M -jar application/build/libs/application-*.jar
--- teamcity-export/src/test/kotlin/org/gradle/devprod/collector/teamcity/TeamcityExportTest.kt
+++ teamcity-export/src/test/kotlin/org/gradle/devprod/collector/teamcity/TeamcityExportTest.kt
@@ -9,7 +9,7 @@ import java.time.temporal.ChronoUnit
 class TeamcityExportTest {
 
     @Test
-    @Disabled("This is a manual test that requires a live tc token")
+//    @Disabled("This is a manual test that requires a live tc token")
     fun `can download GE builds`() {
 
         val loggingRepo = object: Repository {
@@ -24,11 +24,10 @@ class TeamcityExportTest {
 
         }
         val objectMapper = ObjectMapper()
-        val tcService = TeamcityClientService(teamCityApiToken = "", objectMapper)
+        val tcService = TeamcityClientService(teamCityApiToken = System.getenv("TC_TOKEN"), objectMapper)
         val exporter = TeamcityExport(loggingRepo, tcService)
 
-        exporter.loadGeFailedBuilds()
-
+        exporter.loadGbtTriggerBuilds()
     }
 
 }
