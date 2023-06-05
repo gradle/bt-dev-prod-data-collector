778dd69abacf07acec83b19d33c4dbc7ea7b1a0a
Bo Zhang <bo@gradle.com>
2023-06-05 19:58:45 +0800

    Fix failing database cleanup job
    This fixes https://github.com/gradle/bt-dev-prod-data-collector/issues/42

--- ge-export/src/main/kotlin/org/gradle/devprod/collector/enterprise/export/DatabaseCleanupService.kt
+++ ge-export/src/main/kotlin/org/gradle/devprod/collector/enterprise/export/DatabaseCleanupService.kt
@@ -1,7 +1,9 @@
 package org.gradle.devprod.collector.enterprise.export
 
 import org.gradle.devprod.collector.persistence.generated.jooq.Tables.BUILD
+import org.gradle.devprod.collector.persistence.generated.jooq.Tables.FLAKY_TEST_CLASS
 import org.gradle.devprod.collector.persistence.generated.jooq.Tables.LONG_TEST
+import org.gradle.devprod.collector.persistence.generated.jooq.Tables.TEAMCITY_BUILD
 import org.jooq.DSLContext
 import org.springframework.scheduling.annotation.Async
 import org.springframework.scheduling.annotation.Scheduled
@@ -12,11 +14,15 @@ const val BULK_SIZE = 10000
 
 @Component
 class DatabaseCleanupService(private val create: DSLContext) {
-    @Async
-    @Scheduled(fixedDelay = 6 * 60 * 60 * 1000)
-    fun cleanupDb() {
-        val limitDate = OffsetDateTime.now().minusMonths(3)
-        val beforeLimitData = BUILD.BUILD_START.lessOrEqual(limitDate)
+    final var healthy: Boolean = true
+        private set
+
+    private fun deleteInTeamCityBuildTable(date: OffsetDateTime) {
+        create.delete(TEAMCITY_BUILD).where(TEAMCITY_BUILD.FINISHED.lessOrEqual(date))
+    }
+
+    private fun deleteInBuildTable(date: OffsetDateTime) {
+        val beforeLimitData = BUILD.BUILD_START.lessOrEqual(date)
         val oldBuildIdsQuery =
             create
                 .select(BUILD.BUILD_ID)
@@ -30,9 +36,26 @@ class DatabaseCleanupService(private val create: DSLContext) {
             result = oldBuildIdsQuery.fetch(BUILD.BUILD_ID)
 
             create.delete(LONG_TEST).where(LONG_TEST.BUILD_ID.`in`(result)).execute()
+            create.delete(FLAKY_TEST_CLASS).where(FLAKY_TEST_CLASS.BUILD_ID.`in`(result)).execute()
             create.delete(BUILD).where(BUILD.BUILD_ID.`in`(result)).execute()
 
-            println("Deleted ${result.size} old builds")
+            println("Deleted ${result.size} old builds in `build` table")
         } while (result.size == BULK_SIZE)
+    }
+
+    @Async
+    @Scheduled(fixedDelay = 6 * 60 * 60 * 1000)
+    fun cleanupDb() {
+        try {
+            val limitDate = OffsetDateTime.now().minusMonths(3)
+
+            deleteInBuildTable(limitDate)
+            deleteInTeamCityBuildTable(limitDate)
+
+            healthy = true
+        } catch (e: Exception) {
+            healthy = false
+            throw e
+        }
     }
 }
