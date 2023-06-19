28a6f547f98a69fea502130f2238e7115ce0e4d1
Bálint Hegyi <bhegyi@gradle.com>
2023-06-19 10:39:25 +0100

    Format flyway migration, bump version

--- persistence/src/main/resources/db/migration/V1.20__Add_precondition_test_table.sql
+++ persistence/src/main/resources/db/migration/V1.20__Add_precondition_test_table.sql
@@ -1,17 +1,23 @@
 create table precondition_test
 (
     -- The build id, references the build table
-    build_id   varchar(255)  not null,
+    build_id      varchar(255)  not null,
     -- The class name where the precondition test comes from (e.g. LocalPreconditionTest, RemotePreconditionTest)
-    class_name varchar(1024) not null,
+    class_name    varchar(1024) not null,
     -- Comma separated list of preconditions (e.g. "precondition1,precondition2")
     preconditions text[],
     -- True if the test was skipped, false otherwise
-    skipped boolean not null,
+    skipped       boolean       not null,
     -- True if the test failed, false otherwise
-    failed boolean not null,
+    failed        boolean       not null,
+
+    -- Primary key definition
     primary key (build_id, class_name, preconditions),
-    foreign key (build_id) references build,
+    -- We declare build_id as a foreign key. With cascading enabled,
+    -- it will be automatically cleaned up, when the build is deleted
+    foreign key (build_id)
+        references build on delete cascade,
     -- We expect that we always have at least one precondition
-    constraint non_empty_array_constraint check (cardinality(preconditions) > 0)
+    constraint non_empty_array_constraint
+        check (cardinality(preconditions) > 0)
 )
