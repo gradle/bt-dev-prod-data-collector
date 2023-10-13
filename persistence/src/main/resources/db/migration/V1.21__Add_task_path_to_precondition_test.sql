38692b593ad7f76c452b9606cb5e4f3fa6238337
Bo Zhang <bo@gradle.com>
2023-10-13 17:19:35 +0800

    Change primary key

--- persistence/src/main/resources/db/migration/V1.21__Add_task_path_to_precondition_test.sql
+++ persistence/src/main/resources/db/migration/V1.21__Add_task_path_to_precondition_test.sql
@@ -1,2 +1,7 @@
 alter table precondition_test
     add task_path varchar(1024) default null;
+
+alter table precondition_test drop constraint precondition_test_pkey;
+
+alter table precondition_test
+    add constraint primary key (build_id, class_name, preconditions, task_path);
