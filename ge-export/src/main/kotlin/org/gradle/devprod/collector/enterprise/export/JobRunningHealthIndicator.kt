7a22fee569c70e5a9c0b94e7025474e885aaece4
Bo Zhang <bo@gradle.com>
2024-02-05 10:24:06 +0800

    comment

--- ge-export/src/main/kotlin/org/gradle/devprod/collector/enterprise/export/JobRunningHealthIndicator.kt
+++ ge-export/src/main/kotlin/org/gradle/devprod/collector/enterprise/export/JobRunningHealthIndicator.kt
@@ -5,14 +5,14 @@ import org.springframework.boot.actuate.health.Health
 import org.springframework.boot.actuate.health.HealthIndicator
 import org.springframework.stereotype.Component
 
-@Component
-class JobRunningHealthIndicator(private val job: Job) : HealthIndicator {
-    private val messageKey = "Stream to database job"
-
-    override fun health(): Health =
-        if (job.isActive) {
-            Health.up().withDetail(messageKey, "Running").build()
-        } else {
-            Health.down().withDetail(messageKey, "Not Running").build()
-        }
-}
+//@Component
+//class JobRunningHealthIndicator(private val job: Job) : HealthIndicator {
+//    private val messageKey = "Stream to database job"
+//
+//    override fun health(): Health =
+//        if (job.isActive) {
+//            Health.up().withDetail(messageKey, "Running").build()
+//        } else {
+//            Health.down().withDetail(messageKey, "Not Running").build()
+//        }
+//}
